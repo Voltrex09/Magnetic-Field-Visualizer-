b3a80d002b8166d4691913ab1ac37fa7ebfb9a74
Nikolas Prassas <204929811+Voltrex09@users.noreply.github.com>
2026-01-11 17:12:38 +0200

    Add files via upload

--- mag_field_vis.ino
+++ mag_field_vis.ino
@@ -1,22 +1,23 @@
 #include <LedControl.h>
 
-
 #define DIN 11
 #define CLK 13
 #define CS  10
 #define HALL_PIN A0
 #define POT_PIN  A1
-
+#define BUZZER   6
 LedControl lc = LedControl(DIN, CLK, CS, 1);
-
 int centerValue = 512;
+int lastHeight = 0;
+bool fieldDetected = false;
 
 void setup() {
   lc.shutdown(0, false);
   lc.setIntensity(0, 8);
   lc.clearDisplay(0);
 
-  
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, LOW);
   long sum = 0;
   for (int i = 0; i < 100; i++) {
     sum += analogRead(HALL_PIN);
@@ -24,22 +25,40 @@ void setup() {
   }
   centerValue = sum / 100;
 }
+
+void beep(int durationMs) {
+  digitalWrite(BUZZER, HIGH);
+  delay(durationMs);
+  digitalWrite(BUZZER, LOW);
+}
+
 void loop() {
   int hallRaw = analogRead(HALL_PIN);
   int diff = hallRaw - centerValue;
   int potValue = analogRead(POT_PIN);
   int sensitivity = map(potValue, 0, 1023, 10, 200);
+
   lc.clearDisplay(0);
-  lc.setLed(0, 4, 4, true);
+
   int strength = constrain(abs(diff), 0, sensitivity);
   int height = map(strength, 0, sensitivity, 0, 8);
+  int column = (diff > 0) ? 5 : 2;
 
-  int column;
-  if (diff > 0) {
-    column = 5;   
-  } else {
-    column = 2;   
+  if (strength > 2 && !fieldDetected) {
+    beep(120);
+    fieldDetected = true;
   }
+
+  if (strength <= 2) {
+    fieldDetected = false;
+    lastHeight = 0;
+  }
+
+  if (height > lastHeight) {
+    beep(60);
+  }
+
+  lastHeight = height;
 
   for (int row = 7; row >= 8 - height; row--) {
     lc.setLed(0, row, column, true);
